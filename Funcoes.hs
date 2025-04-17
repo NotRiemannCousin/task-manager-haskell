4c534702fc85bea7406b214ac8fbf2d7c73aaa12
Leonardo Beloube <122946568+LIBYZz@users.noreply.github.com>
2025-04-17 00:14:19 -0300

    Update Funcoes.hs
    adicionei funções básicas

--- Funcoes.hs
+++ Funcoes.hs
@@ -1,6 +1,25 @@
 module Funcoes where
  import Tipos
 
+-- FUNÇÕES BÁSICAS
+adicionarTarefa :: Tarefa -> [Tarefa] -> Either String [Tarefa]
+adicionarTarefa novaTarefa tarefas
+    | [t | t <- tarefas, idTarefa t == idTarefa novaTarefa] /= [] = Left "Erro!Tarefa já registrada"
+    | otherwise = Right (novaTarefa : tarefas)
+
+removerTarefa :: Int -> [Tarefa] -> Either String [Tarefa]
+removerTarefa id tarefas =
+    if any (\t -> idTarefa t == id) tarefas
+       then Right (filter (\t -> idTarefa t /= id) tarefas)
+       else Left "Erro! Tarefa não encontrada"
+
+marcarConcluida :: Int -> [Tarefa] -> Either String [Tarefa]
+marcarConcluida id tarefas =
+    if any (\t -> idTarefa t == id) tarefas
+       then Right (map (\t -> if idTarefa t == id then t {status = Concluida} else t) tarefas)
+       else Left "Erro! Tarefa não encontrada"
+-- FUNÇÕES BÁSICAS
+
 -- filtra a lista "lista" com a categoria "cat"
 
 listarPorCategoria :: Categoria -> [Tarefa] -> [Tarefa]
