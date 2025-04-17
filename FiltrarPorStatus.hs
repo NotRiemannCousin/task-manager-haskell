6786834b800cc0c7389262e1f02a839dc57b491f
Leonardo Beloube <122946568+LIBYZz@users.noreply.github.com>
2025-04-17 00:12:58 -0300

    Delete FiltrarPorStatus.hs
    tirei arquivos soltos

--- FiltrarPorStatus.hs
+++
@@ -1,4 +0,0 @@
-filtrarPorStatus :: Status -> [Tarefa] -> [Tarefa]
-filtrarPorStatus estado lista = filter (\t -> status t == estado) lista
--- filter nesse caso tem como objetivo filtrar os elementos de um certo
--- status t que são iguais a variavel estado da lista de tarefas principal
