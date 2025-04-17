bb3891c1f75f3a38b99f5c1806f22a938897d640
Leonardo Beloube <122946568+LIBYZz@users.noreply.github.com>
2025-04-17 00:11:09 -0300

    Update Funcoes.hs
    filtrarPorStatus

--- Funcoes.hs
+++ Funcoes.hs
@@ -1,14 +1,18 @@
 module Funcoes where
  import Tipos
 
- -- filtra a lista "lista" com a categoria "cat"
+-- filtra a lista "lista" com a categoria "cat"
 
 listarPorCategoria :: Categoria -> [Tarefa] -> [Tarefa]
 listarPorCategoria cat lista = filter (\t -> categoria t == cat) lista
-
 
 -- filtra a lista "lista" com a prioridade "pri"
 
 listarPorPrioridade :: Prioridade -> [Tarefa] -> [Tarefa]
 listarPorPrioridade pri lista = filter (\t -> prioridade t == pri) lista
 
+
+-- filter nesse caso tem como objetivo filtrar os elementos de um certo
+-- status t que são iguais a variavel estado da lista de tarefas principal
+filtrarPorStatus :: Status -> [Tarefa] -> [Tarefa]
+filtrarPorStatus estado lista = filter (\t -> status t == estado) lista
