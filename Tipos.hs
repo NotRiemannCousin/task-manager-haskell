6d6dc71ed5b1e5732978ba6164345e55c851045c
Iury <Iuryhattori.prog@gmail.com>
2025-04-16 22:06:45 -0300

    Create Tipos.hs

---
+++ Tipos.hs
@@ -0,0 +1,18 @@
+module Tipos where
+ import Data.Time.Calendar (Day)
+
+ data Status = Pendente | Concluída deriving (Show, Eq)
+
+ data Prioridade = Baixa | Media | Alta deriving (Show, Eq, Ord)
+
+ data Categoria = Trabalho | Estudos | Pessoal | Outro deriving (Show,Eq)
+
+ data Tarefa = Tarefa
+  { idTarefa    :: Int
+  , descricao   :: String
+  , status      :: Status
+  , prioridade  :: Prioridade
+  , categoria   :: Categoria
+  , prazo       :: Maybe Day
+  , tags       :: [String]
+  } deriving (Show, Eq)
