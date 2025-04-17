4741f7686643ad787f7813ae6ad2fdfceeb8790b
Leonardo Beloube <122946568+LIBYZz@users.noreply.github.com>
2025-04-17 00:10:22 -0300

    Update Funcoes.hs
    listarPorCategoria e listarPorPrioridade

--- Funcoes.hs
+++ Funcoes.hs
@@ -1,2 +1,14 @@
 module Funcoes where
  import Tipos
+
+ -- filtra a lista "lista" com a categoria "cat"
+
+listarPorCategoria :: Categoria -> [Tarefa] -> [Tarefa]
+listarPorCategoria cat lista = filter (\t -> categoria t == cat) lista
+
+
+-- filtra a lista "lista" com a prioridade "pri"
+
+listarPorPrioridade :: Prioridade -> [Tarefa] -> [Tarefa]
+listarPorPrioridade pri lista = filter (\t -> prioridade t == pri) lista
+
