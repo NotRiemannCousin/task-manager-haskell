118389980da79216b0b04ef10c989d6246558a7d
Leonardo Beloube <122946568+LIBYZz@users.noreply.github.com>
2025-04-17 15:38:02 -0300

    Update main.hs
    criei o menu na main

--- main.hs
+++ main.hs
@@ -5,5 +5,28 @@ module Main where
  
  main :: IO ()
  main = do
-    putStrLn "___________TAREFAS_____________"
+ -- menu de ações --
+    putStr "- O que gostaria de realizar?:\n "
+    putStr "  1  > adicionar nova tarefa\n"
+    putStr "   2  > remover uma tarefa\n"
+    putStr "   3  > marcar uma tarefa como concluida\n"
+    putStr "   4  > listar tarefas por categoria\n"
+    putStr "   5  > listar tarefas por prioridade\n"
+    putStr "   6  > ordenar tarefas por prioridade\n"
+    putStr "   7  > filtrar tarefas por status\n"
+    putStr "   8  > buscar por palavra chave\n"
+    putStr "   9  > calcular dias restantes\n"
+    putStr "  10  > filtrar tarefa por tag\n"
+    putStr "  11  > nuvem de tags\n"
+    putStr "- Digite o numero da ação: "
+ -- menu de ações --
+    
+ -- resposta --
+    input <- getLine
+ -- resposta --
+
+ -- lê a resposta --
+    let result = read input :: Int
+    print result
+ -- lê a resposta --
 
