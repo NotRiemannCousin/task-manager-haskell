f5eafa129a773cf5fe084370b2d8b0e64b0e34cf
Marcelo <marcelohenrique8822@gmail.com>
2025-04-20 03:32:09 -0300

    fixes on menu

--- Tipos.hs
+++ Tipos.hs
@@ -2,11 +2,11 @@ module Tipos where
 
  import Data.Time.Calendar (Day)
 
- data Status = Pendente | Concluída deriving (Show, Eq)
+ data Status = Pendente | Concluída deriving (Read, Show, Eq)
 
- data Prioridade = Baixa | Media | Alta deriving (Show, Eq, Ord)
+ data Prioridade = Baixa | Media | Alta deriving (Read, Show, Eq, Ord)
 
- data Categoria = Trabalho | Estudos | Pessoal | Outro deriving (Show, Eq)
+ data Categoria = Trabalho | Estudos | Pessoal | Outro deriving (Read, Show, Eq)
 
  data Tarefa = Tarefa
   { idTarefa    :: Int
@@ -16,4 +16,4 @@ module Tipos where
   , categoria   :: Categoria
   , prazo       :: Maybe Day
   , tags       :: [String]
-  } deriving (Show, Eq)
+  } deriving (Read, Show, Eq)
--- main.hs
+++ main.hs
@@ -1,86 +1,32 @@
 module Main where
 
 import Data.Time.Calendar
-import Funcoes
+import Data.Time
+import Data.List
 import System.IO
+import Funcoes
 import Tipos
-import Main (tarefas)
-import Funcoes (
-    adicionarTarefa,
-    removerTarefa,
-    marcarConcluida,
-    listarPorCategoria,
-    listarPorPrioridade,
-    ordenarPorPrioridade,
-    filtrarPorStatus,
-    buscarPorPalavraChave,
-    calcularDiasRestantes,
-    filtrarPorTag,
-    nuvemDeTags)
-
-
-carregarDeArquivo :: IO [Tarefa]
-carregarDeArquivo = do
-    putStrLn "Carregando tarefas..."
-    contents <- readFile "tarefas.txt"
-    return (read contents :: [Tarefa])
 
 
 main :: IO ()
 main = do
-    tarefas <- carregarDeArquivo
+    hSetBuffering stdout NoBuffering
+
+    let tarefas = []
     res <- menu tarefas
     writeFile "tarefas.txt" (show res)
 
 
 
-
-
-
-
-
-menu :: [Tarefa] -> IO [Tarefa]
-menu tarefas = do
-    -- menu de ações --
-    putStr "- O que gostaria de realizar?:\n "
-    putStr "   1  > adicionar nova tarefa\n"
-    putStr "   2  > remover uma tarefa\n"
-    putStr "   3  > marcar uma tarefa como concluida\n"
-    putStr "   4  > listar tarefas por categoria\n"
-    putStr "   5  > listar tarefas por prioridade\n"
-    putStr "   6  > ordenar tarefas por prioridade\n"
-    putStr "   7  > filtrar tarefas por status\n"
-    putStr "   8  > buscar por palavra chave\n"
-    putStr "   9  > verificar atrasos\n"
-    putStr "  10  > calcular dias restantes\n"
-    putStr "  11  > filtrar tarefa por tag\n"
-    putStr "  12  > nuvem de tags\n"
-    putStr "  13  > Sair\n"
-    putStr "- Digite o numero da ação: "
-    -- resposta --
-    input <- getLine
-    let result = read input :: Int
-    -- lê a resposta --
-    case result of
-        1  -> menu (adicionarTarefaMenu       tarefas)
-        2  -> menu (removerTarefaMenu         tarefas)
-        3  -> menu (marcarConcluidaMenu       tarefas)
-        4  -> menu (listarPorCategoriaMenu    tarefas)
-        5  -> menu (listarPorPrioridadeMenu   tarefas)
-        6  -> menu (ordenarPorPrioridadeMenu  tarefas)
-        7  -> menu (filtrarPorStatusMenu      tarefas)
-        8  -> menu (buscarPorPalavraChaveMenu tarefas)
-        9  -> menu (verificarAtrasos          tarefas)
-        10 -> menu (calcularDiasRestantesMenu tarefas)
-        11 -> menu (filtrarPorTagMenu         tarefas)
-        12 -> menu (nuvemDeTagsMenu           tarefas)
-        13 -> return tarefas
-        _ -> do
-            putStr "Comando inválido!\n"
-            menu tarefas
-
-
-
+inputString :: String -> IO String
+inputString text = do
+    putStr text
+    line <- getLine
+    if line /= ""
+       then return line
+       else do
+        putStr "String não vazia!\n"
+        input text
 
 input :: Read a => String -> IO a
 input text = do
@@ -88,90 +34,170 @@ input text = do
     line <- getLine
     case reads line of
         [(x, "")] -> return x
-        _         -> do
+        _      -> do
             putStr "Comando inválido!\n"
             input text
 
 
 
-adicionarTarefaMenu :: IO [Tarefa]
-adicionarTarefaMenu = do
-    id         <- input "Digite o id da tarefa: " :: IO Int
-    titulo     <- input "Digite o titulo da tarefa: " :: IO String
-    descricao  <- input "Digite a descricao da tarefa: " :: IO String
-    categoria  <- input "Digite a categoria da tarefa: " :: IO String
-    prioridade <- input "Digite a prioridade da tarefa: " :: IO String
-    status     <- input "Digite o status da tarefa: " :: IO String
-    prazo      <- input "Digite o prazo da tarefa: " :: IO Day
-    tagsStr    <- input "Digite as tags da tarefa (separadas por espaço): " :: IO String
-    adicionarTarefa ++ Tarefa id titulo readstatus readprioridade readcategoria prazo (split ' ' tagsStr)
 
-removerTarefaMenu :: Tarefa -> IO [Tarefa]
+menu :: [Tarefa] -> IO [Tarefa]
+menu tarefas = do
+    -- menu de ações --
+    putStrLn $ replicate 30 '\n' -- limpa tudo
+    putStrLn "- O que gostaria de realizar?:"
+    putStrLn "   1  > adicionar nova tarefa"
+    putStrLn "   2  > remover uma tarefa"
+    putStrLn "   3  > marcar uma tarefa como concluida"
+    putStrLn "   4  > listar tarefas por categoria"
+    putStrLn "   5  > listar tarefas por prioridade"
+    putStrLn "   6  > ordenar tarefas por prioridade"
+    putStrLn "   7  > filtrar tarefas por status"
+    putStrLn "   8  > buscar por palavra chave"
+    putStrLn "   9  > verificar atrasos"
+    putStrLn "  10  > calcular dias restantes"
+    putStrLn "  11  > filtrar tarefa por tag"
+    putStrLn "  12  > nuvem de tags"
+    putStrLn "  13  > Sair"
+    putStrLn "- Digite o numero da ação: "
+    -- o getChar segura o terminal até uma linha ser precionada, dps vai para o menu
+    let nextIteration ts = getChar >> menu ts
+    -- resposta --
+    input <- getLine
+    case reads input of
+        [(1  :: Int, "")] -> adicionarTarefaMenu tarefas       >>= nextIteration
+        [(2  :: Int, "")] -> removerTarefaMenu tarefas         >>= nextIteration
+        [(3  :: Int, "")] -> marcarConcluidaMenu tarefas       >>= nextIteration
+        [(4  :: Int, "")] -> listarPorCategoriaMenu tarefas    >>= nextIteration
+        [(5  :: Int, "")] -> listarPorPrioridadeMenu tarefas   >>= nextIteration
+        [(6  :: Int, "")] -> ordenarPorPrioridadeMenu tarefas  >>= nextIteration
+        [(7  :: Int, "")] -> filtrarPorStatusMenu tarefas      >>= nextIteration
+        [(8  :: Int, "")] -> buscarPorPalavraChaveMenu tarefas >>= nextIteration
+        [(9  :: Int, "")] -> verificarAtrasosMenu tarefas      >>= nextIteration
+        [(10 :: Int, "")] -> calcularDiasRestantesMenu tarefas >>= nextIteration
+        [(11 :: Int, "")] -> filtrarPorTagMenu tarefas         >>= nextIteration
+        [(12 :: Int, "")] -> nuvemDeTagsMenu tarefas           >>= nextIteration
+        [(13 :: Int, "")] -> return tarefas
+        _ -> do
+            putStr "Comando inválido!\n"
+            nextIteration tarefas
+            getChar
+            menu tarefas
+
+
+
+
+adicionarTarefaMenu :: [Tarefa] -> IO [Tarefa]
+adicionarTarefaMenu tarefas = do
+    id         <- input       "Digite o id da tarefa: " :: IO Int
+    titulo     <- inputString "Digite o titulo da tarefa: "
+    descricao  <- inputString "Digite a descricao da tarefa: "
+    categoria  <- input       "Digite a categoria da tarefa: " :: IO Categoria
+    prioridade <- input       "Digite a prioridade da tarefa: " :: IO Prioridade
+    status     <- input       "Digite o status da tarefa: " :: IO Status
+    prazo      <- input       "Digite o prazo da tarefa: " :: IO (Maybe Day)
+    tagsStr    <- inputString "Digite as tags da tarefa (separadas por espaço): "
+
+    let tarefa = Tarefa id titulo status prioridade categoria prazo (words tagsStr)
+
+    case adicionarTarefa tarefa tarefas of
+        Left msg -> do
+            putStrLn msg
+            return tarefas
+        Right tarefas -> do
+            putStrLn "Tarefa adicionada com sucesso!"
+            return tarefas
+
+removerTarefaMenu :: [Tarefa] -> IO [Tarefa]
 removerTarefaMenu tarefas = do
     id <- input "Digite o id da tarefa a ser removida: " :: IO Int
-    removerTarefa id tarefas
+    case removerTarefa id tarefas of
+        Left msg -> do
+            putStrLn msg
+            return tarefas
+        Right tarefas -> do
+            putStrLn "Tarefa removida com sucesso!"
+            return tarefas
 
-marcarConcluidaMenu :: Tarefa -> IO [Tarefa]
+marcarConcluidaMenu :: [Tarefa] -> IO [Tarefa]
 marcarConcluidaMenu tarefas = do
     id <- input "Digite o id da tarefa a ser concluida: " :: IO Int
-    marcarConcluida id tarefas
+    let res = marcarConcluida id tarefas
+    case res of
+        Left msg      -> do
+            putStrLn msg
+            return tarefas
+        Right tarefas -> do
+            putStrLn "Tarefa concluida com sucesso!"
+            return tarefas
 
 
-listarPorCategoriaMenu :: Tarefa -> IO [Tarefa]
+listarPorCategoriaMenu :: [Tarefa] -> IO [Tarefa]
 listarPorCategoriaMenu tarefas = do
-    categoria <- input "Digite a categoria da tarefa: " :: IO String
+    categoria <- input "Digite a categoria da tarefa: " :: IO Categoria
     print $ listarPorCategoria categoria tarefas -- lembrar de imprimir bonitinho dps
-    tarefas -- ou o resultado da função?
+    return tarefas
 
-listarPorPrioridadeMenu :: Tarefa -> IO [Tarefa]
+listarPorPrioridadeMenu :: [Tarefa] -> IO [Tarefa]
 listarPorPrioridadeMenu tarefas = do
-    prioridade <- input "Digite a prioridade da tarefa: " :: IO String
+    prioridade <- input "Digite a prioridade da tarefa: " :: IO Prioridade
     print $ listarPorPrioridade prioridade tarefas -- lembrar de imprimir bonitinho dps
-    tarefas -- ou o resultado da função?
+    return tarefas
 
-ordenarPorPrioridadeMenu :: Tarefa -> IO [Tarefa]
+ordenarPorPrioridadeMenu :: [Tarefa] -> IO [Tarefa]
 ordenarPorPrioridadeMenu tarefas = do
     putStrLn "Ordenando tarefas..."
     print $ ordenarPorPrioridade tarefas -- lembrar de imprimir bonitinho dps
-    tarefas -- ou o resultado da função?
+    return tarefas
 
-filtrarPorStatusMenu :: Tarefa -> IO [Tarefa]
+filtrarPorStatusMenu :: [Tarefa] -> IO [Tarefa]
 filtrarPorStatusMenu tarefas = do
-    status <- input "Digite o status da tarefa: " :: IO String
+    status <- input "Digite o status da tarefa: " :: IO Status
     print $ filtrarPorStatus status tarefas -- lembrar de imprimir bonitinho dps
-    tarefas -- ou o resultado da função?
+    return tarefas
 
 
-buscarPorPalavraChaveMenu :: Tarefa -> IO [Tarefa]
+buscarPorPalavraChaveMenu :: [Tarefa] -> IO [Tarefa]
 buscarPorPalavraChaveMenu tarefas = do
-    palavraChave <- input "Digite a palavra chave: " :: IO String
+    palavraChave <- inputString "Digite a palavra chave: "
     print $ buscarPorPalavraChave palavraChave tarefas -- lembrar de imprimir bonitinho dps
-    tarefas -- ou o resultado da função?
+    return tarefas
 
-verificarAtrasosMenu :: Tarefa -> IO [Tarefa]
+verificarAtrasosMenu :: [Tarefa] -> IO [Tarefa]
 verificarAtrasosMenu tarefas = do
+    currentTime <- getCurrentTime
     putStrLn "Atrasados:"
-    print $ verificarAtrasos tarefas $ getCurrentTime . toGregorian . utctDay
-    tarefas
+    print $ verificarAtrasos tarefas (utctDay currentTime)  -- lembrar de imprimir bonitinho dps (utctDay toGregorian.
+    return tarefas
 
-calcularDiasRestantesMenu :: Tarefa -> IO [Tarefa]
+calcularDiasRestantesMenu :: [Tarefa] -> IO [Tarefa]
 calcularDiasRestantesMenu tarefas = do
     id <- input "Digite o id da tarefa: " :: IO Int
-    dias <- calcularDiasRestantes (find id tarefa) tarefas
-    case dias of
-        Nothing -> putStr "Essa tarefa não tem prazo"
-        _       -> putStr concat $ "Dias restantes: " (show dias)
-    tarefas
+    let tarefa = findTarefa id tarefas
+    case tarefa of
+        Nothing -> putStrLn "Tarefa não encontrada"
+        Just t -> do
+            currentTime <- getCurrentTime
+            let dias = calcularDiasRestantes t (utctDay currentTime)
+            case dias of
+                Nothing -> putStrLn "Essa tarefa não tem prazo"
+                Just d  -> putStrLn $ "Dias restantes: " ++ show d
+    return tarefas
+    where
+        findTarefa _ [] = Nothing
+        findTarefa id (t:ts)
+            | idTarefa t == id = Just t
+            | otherwise = findTarefa id ts
 
-filtrarPorTagMenu :: Tarefa -> IO [Tarefa]
+filtrarPorTagMenu :: [Tarefa] -> IO [Tarefa]
 filtrarPorTagMenu tarefas = do
-    tag <- input "Digite a tag a ser procurada: " :: IO Int
-    print filtrarPorTag tarefas -- lembrar de imprimir bonitinho dps
-    tarefas -- ou o resultado da função?
+    tag <- inputString "Digite a tag a ser procurada: "
+    print $ filtrarPorTag tag tarefas -- lembrar de imprimir bonitinho dps
+    return tarefas
 
 
-nuvemDeTagsMenu :: Tarefa -> IO [Tarefa]
+nuvemDeTagsMenu :: [Tarefa] -> IO [Tarefa]
 nuvemDeTagsMenu tarefas = do
     putStrLn "Tags:"
     print $ nuvemDeTags tarefas -- lembrar de imprimir bonitinho dps
-    tarefas
+    return tarefas
