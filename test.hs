c6ca2a75afc5be6088c4604cd58d58cd6c90d170
Iury <Iuryhattori.prog@gmail.com>
2025-04-22 20:59:32 -0300

    Update test.hs

--- test.hs
+++ test.hs
@@ -1,13 +1,24 @@
 module Testes (runTests) where
 
 import Test.QuickCheck
+import Data.Time.Calendar (Day, diffDays, fromGregorian)
 import Tipos
 import Funcoes
+
+instance Arbitrary Day where
+    arbitrary = do
+        ano <- choose (2000, 2030)
+        mes <- choose (1, 12)
+        dia <- case () of
+            _ | mes == 2 -> choose (1, 28)
+              | mes `elem` [4,6,9,11] -> choose (1, 30)
+              | otherwise -> choose (1, 31)
+        return $ fromGregorian ano mes dia
 
 instance Arbitrary Status where
     arbitrary = elements [Pendente, Concluída] -- usamos a função elements para sortear aleatoriamente 
 
-instance Arbitrary Propriedades where
+instance Arbitrary Prioridade where
     arbitrary = elements [Baixa, Media, Alta]
 
 instance Arbitrary Categoria where
@@ -22,7 +33,7 @@ instance Arbitrary Tarefa where
         cat <- arbitrary -- onde cat representa a categoria
         praz <- arbitrary:: Gen (Maybe Day) -- onde praz representa o prazo
         tg <- arbitrary:: Gen [String] -- onde tg representa a tag, e gerara uma lista de string aleatorias
-        return $ Tarefa id des stat cat praz tg 
+        return $ Tarefa id des stat pri cat praz tg
 
 propAddTarefaNp :: Tarefa -> [Tarefa] -> Property
 propAddTarefaNp x y = 
@@ -61,8 +72,8 @@ proplistarPorCategoriaNp x y =
 
 proplistarPorCategoriaProblem :: Categoria -> [Tarefa] -> Property
 proplistarPorCategoriaProblem x y =
-    null(filter (\t -> categoria t == x) y) ==>
-        listarPorCategoria x y == Left "Erro! Categoria não encontrada!"
+    null [t | t <- y, categoria t == x] ==>
+        listarPorCategoria x y == []
 
 propfiltrarPorStatusNp :: Status->[Tarefa]->Property
 propfiltrarPorStatusNp x y =
@@ -72,10 +83,10 @@ propfiltrarPorStatusNp x y =
 propfiltrarporStatusProblem :: Status -> [Tarefa] -> Property
 propfiltrarporStatusProblem x y =
     null (filter (\t -> status t == x) y) ==>
-        filtrarPorStatus x y == Left "Erro! Status não encontrado"
+        filtrarPorStatus x y == []
 
 propbuscarPorPalavraChaveNp :: String -> [Tarefa] -> Property
-probuscarPorPalavraChaveNp x y =
+propbuscarPorPalavraChaveNp x y =
     not (null (filter (\t -> transformarMinuscula (descricao t ) == transformarMinuscula x) y)) ==> 
         buscarPorPalavraChave x y == filter(\t -> transformarMinuscula (descricao t) == transformarMinuscula x) y
 
@@ -84,42 +95,42 @@ propbuscarPorPalavraChaveProblem x y =
     null (filter (\t -> transformarMinuscula (descricao t ) == transformarMinuscula x) y) ==>
         buscarPorPalavraChave x y == []
 
-propCalcularDiasRestantesNp :: Tarefa -> Day -> Property -- Testado!
+propCalcularDiasRestantesNp :: Tarefa -> Day -> Property 
 propCalcularDiasRestantesNp x dia = 
     prazo x /= Nothing ==> -- executa caso tenha um prazo definido
         calcularDiasRestantes x dia == (maybe Nothing (\p -> Just (fromIntegral (diffDays p dia)))) (prazo x)
 
-propCalcularDiasRestantesProblem :: Tarefa -> Day -> Property -- 
+propCalcularDiasRestantesProblem :: Tarefa -> Day -> Property
 propCalcularDiasRestantesProblem x dia =
     prazo x == Nothing ==> -- executa caso a tarefa não possua um prazo definido 
         calcularDiasRestantes x dia == Nothing
 
-propVerificaAtrasosNp :: [Tarefa] -> Day -> Property -- Testado !
+propVerificaAtrasosNp :: [Tarefa] -> Day -> Property 
 propVerificaAtrasosNp x dia =
     not (null x) ==> -- somente executa se a lista não for vazia
         all (\t -> case prazo t of
-            Just p -> if p < dia then t `elem` verificaAtrasos x dia else True
+            Just p -> if p < dia then t `elem` verificarAtrasos x dia else True
             Nothing -> True) x
 
 
-propVerificaAtrasosProblem :: [Tarefa] -> Day -> Property -- Testado!!
+propVerificaAtrasosProblem :: [Tarefa] -> Day -> Property 
 propVerificaAtrasosProblem x dia =
     not (null x) ==>
         all (\t -> case prazo t of
-                Just p -> if p > dia then notElem t (verificaAtrasos x dia) else True
-                Nothing -> notElem t (verificaAtrasos x dia)) x
+                Just p -> if p > dia then notElem t (verificarAtrasos x dia) else True
+                Nothing -> notElem t (verificarAtrasos x dia)) x
 
-propfiltrarPorTagNp :: String -> [Tarefa] -> Property -- Parcialmente Testado!!
+propfiltrarPorTagNp :: String -> [Tarefa] -> Property 
 propfiltrarPorTagNp x y =
     not (null y) ==> -- o codigo é executado quando a lista tarefas nao é vazia
         all (\t -> any (==x) (tags t)) (filtrarPorTag x y)
 
-propFiltrarPorTagProblem :: String -> [Tarefa] -> Property -- Parcialmente Testado !!
+propFiltrarPorTagProblem :: String -> [Tarefa] -> Property 
 propFiltrarPorTagProblem x y =
     all (\t -> not (any (== x) (tags t))) y ==>
         filtrarPorTag x y == []
 
-propNuvemDeTagsNp :: [Tarefa] -> Property -- Parcialmente testado!!
+propNuvemDeTagsNp :: [Tarefa] -> Property 
 propNuvemDeTagsNp y = 
     not (null y) ==>
         all (\(tag, count) -> count == length (filter (== tag) (concatMap tags y))) (nuvemDeTags y)
