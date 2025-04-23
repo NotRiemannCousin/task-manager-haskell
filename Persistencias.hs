29c06328da8647345266de5e406041d31782199b
Marcelo <marcelohenrique8822@gmail.com>
2025-04-22 23:14:57 -0300

    forcing evaluation of the whole file

--- Persistencias.hs
+++ Persistencias.hs
@@ -7,18 +7,20 @@ module Persistencias (
  import System.IO
  import Data.Maybe (mapMaybe)
  import Text.Read (readMaybe)
+ import Control.Exception (evaluate)
 
  stringParaTarefa :: String -> Maybe Tarefa
  stringParaTarefa = readMaybe
 
  carregarDeArquivo :: FilePath -> IO [Tarefa]
  carregarDeArquivo ca = do
-     co <- readFile ca
-     let l = lines co
-         tarefas = mapMaybe stringParaTarefa l
-     return tarefas
+    co <- readFile ca
+    let l = lines co
+        tarefas = mapMaybe stringParaTarefa l
+    evaluate (length tarefas)
+    return tarefas
 
  salvarEmArquivo :: FilePath -> [Tarefa] -> IO ()
  salvarEmArquivo ca co = do
-     withFile ca WriteMode $ \handle -> do
-         mapM_ (hPutStrLn handle.show) co
+    withFile ca WriteMode $ \handle -> do
+        mapM_ (hPutStrLn handle.show) co
